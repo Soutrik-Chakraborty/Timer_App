b93ac5cc6810b8642cba75fc195fff9b0c9c84a6
Soutrik Chakraborty <soutrikchakraborty31@gmail.com>
2020-06-03 12:36:13 +0530

    User Interface done!

---
+++ app/src/main/java/soutrik/whatevervalue/com/timerapp/TimerActivity.kt
@@ -0,0 +1,16 @@
+package soutrik.whatevervalue.com.timerapp
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_timer.*
+
+class TimerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Timer App"
+    }
+}
